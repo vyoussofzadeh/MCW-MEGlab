c4db5863474f2fb8ac7e1b62c201a97eedd354ff
Youssofzadeh <vyoussofzadeh@mcw.edu>
2022-04-25 15:42:09 -0500

    fieldtrip function

---
+++ FT_fucntions/mcw_surfplot.m
@@ -0,0 +1,17 @@
+function mcw_surfplot(cfg,surf,source)
+
+% Author: vyoussofzadeh
+% update: 04/25/22
+
+figure,
+set(gcf, 'Position', cfg.position);
+views = cfg.view;
+
+for i=1:length(views)
+    subplot(1,length(views),i)
+    handles = ft_plot_mesh(surf, 'vertexcolor', source);
+    colorbar off
+    axis tight
+    view(cfg.view(i,:));
+end
+end
