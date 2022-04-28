90de9d6f3c27b44162540a3095dbfe4de24a21c4
Youssofzadeh <vyoussofzadeh@mcw.edu>
2022-04-28 16:38:00 -0500

    Update DipEdit.m

--- Other_implementations/Dipole editting in BS/DipEdit.m
+++ Other_implementations/Dipole editting in BS/DipEdit.m
@@ -1,8 +1,23 @@
-test2.DipoleNames = test2.DipoleNames(1:20);
-test2.Dipole = test2.Dipole(1:20);
-test2.Comment = 'dipole test';
-% A = load('dipoles_Run02_Run03_Run.mat');
+clc, clear,
+Path =  '/MEG_data/epilepsy/garcia_brian/brainstorm_db/data';
+Name =  'garcia_brian/Run03_spont_supine_raw_sss_ecgClean_raw/dipoles_Run03_event1002.mat';
 
+fname = fullfile(Path,Name);
+Dip_org = load(fname);
 
-savetag = 'dipoles_Run02_Run03_Run_edit';
-save(fullfile(savetag),'-struct', 'test2'),
+L  = length(Dip_org.Dipole);
+disp(['L=', num2str(L)]);
+% dipin = input('Dipole inputs in range, e.g, [1,200] ');
+dipin  = [1, round(L/2)]
+
+Dip_updt = Dip_org;
+Dip_updt.DipoleNames = Dip_updt.DipoleNames(1:end-1);
+% Dip_updt.DipoleNames = Dip_updt.DipoleNames(dipin(1):dipin(2));
+% Dip_updt.Dipole = Dip_updt.Dipole(dipin(1):dipin(2));
+Dip_updt.Dipole = Dip_updt.Dipole;
+Dip_updt.Comment = 'dipole update';
+
+idx = strfind(fname,'/');
+cd(fname(1:idx(end)))
+savetag = [fname(idx(end)+1:end-4), '_updt.mat'];
+save(fullfile(savetag),'-struct', 'Dip_updt'),
