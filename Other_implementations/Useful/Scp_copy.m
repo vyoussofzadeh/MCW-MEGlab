490b454d1f4ec0ab9f6dc5b14e3b1326fbe129b1
vyoussofzadeh <vyoussofzadeh@mcw.edu>
2022-09-23 10:07:19 -0500

    Scp copy was added

---
+++ Other_implementations/Useful/Scp_copy.m
@@ -0,0 +1,5 @@
+
+
+% cd ('/MEG_data/Research_studies/Epil_annotated_data')
+command = 'scp -r /MEG_data/MRI_database/epilepsy/DEJESUS_Elias_T1_AxBravo/nii vyoussofzadeh@squiggles.rcc.mcw.edu:/data/MEG/Vahab/';
+system(command)
