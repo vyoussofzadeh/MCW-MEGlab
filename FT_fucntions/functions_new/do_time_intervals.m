a41afd1d87fe2755f4c9b38713500a39074c4b7c
vyoussofzadeh <vyoussofzadeh@squiggles.rcc.mcw.edu>
2024-02-29 17:04:48 -0600

    Update

--- FT_fucntions/functions_new/do_time_intervals.m
+++ FT_fucntions/functions_new/do_time_intervals.m
@@ -13,10 +13,10 @@ function [wi]  = do_time_intervals(cfg_main)
 % % length(wi)
 
 
-strt = cfg_main.strt; % strt = 0 sec.
-spt = cfg_main.spt; % spt = 2 sec.
-overlap = cfg_main.overlap; % overlap = 0.01 or 0 for no overlap
-linterval = cfg_main.linterval; % linterval = duration of each interval
+strt = cfg_main.strt; % strt = -0.5 sec.
+spt = cfg_main.spt; % spt = 0 sec.
+overlap = cfg_main.overlap; % overlap = 0;
+linterval = cfg_main.linterval; % linterval = 0.25 sec.
 
 wi = []; 
 w1 = strt; 
@@ -24,15 +24,11 @@ l = linterval;
 ov = overlap; 
 j = 1;
 
-% Adjust the loop to work with or without overlap
 while w1 + l <= spt
     wi(j,:) = [w1, w1 + l]; 
     j = j + 1;
-    if ov > 0
-        w1 = w1 + ov; % If overlap is specified, increment by overlap
-    else
-        w1 = w1 + l; % If no overlap, increment by the interval length
-    end
+    w1 = w1 + (ov > 0) * ov + (ov <= 0) * l; % Adjust w1 based on overlap presence
 end
+
 
 end
