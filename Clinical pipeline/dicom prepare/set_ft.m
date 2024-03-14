a22cb34f352b2a8bc757e0f842821889df62224a
vyoussofzadeh <vyoussofzadeh@mcw.edu>
2024-03-14 10:44:24 -0500

    Update

--- Clinical pipeline/dicom prepare/set_ft.m
+++ Clinical pipeline/dicom prepare/set_ft.m
@@ -2,8 +2,9 @@ function set_ft(path_tools)
 
 restoredefaultpath
 % ft_path = fullfile(path_tools,'/fieldtrip-20161201');
-ft_path = fullfile(path_tools,'/fieldtrip_20190419');
-addpath(fullfile(path_tools,'/fieldtrip_20190419/external/spm8'));
+% ft_path = fullfile(path_tools,'/fieldtrip_20190419');
+% addpath(fullfile(path_tools,'/fieldtrip_20190419/external/spm8'));
+ft_path = fullfile(path_tools,'/fieldtrip_2022');
 addpath(ft_path);
 ft_defaults
 
