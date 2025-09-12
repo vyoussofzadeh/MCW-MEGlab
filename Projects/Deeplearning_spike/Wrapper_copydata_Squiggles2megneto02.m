8d8815b7f1481c2135a29696aade4bbec60e2de2
vyoussofzadeh <vyoussofzadeh@mcw.edu>
2025-09-12 13:54:22 -0500

    AHW updates

---
+++ Projects/Deeplearning_spike/Wrapper_copydata_Squiggles2megneto02.m
@@ -0,0 +1,14 @@
+%% Run this in megneto,
+
+rsync -avhz --progress \
+  vyoussofzadeh@squiggles.rcc.mcw.edu:/data/MEG/Research/SpikeDetection/Spike_times/ \
+  /MEG_data/AHW_SpikeAnalysis/MEG_data/tsss/Spike_times/
+  
+  
+rsync -avhz --progress \
+  vyoussofzadeh@squiggles.rcc.mcw.edu:/data/MEG/Research/SpikeDetection/tsss_ecgClean_Data/ \
+  /MEG_data/AHW_SpikeAnalysis/MEG_data/tsss/sss/
+
+
+  
+  
